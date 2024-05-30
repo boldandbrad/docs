d27f91310989ff823e2373747e000ef9cf0f30ad
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-05-29 21:21:33 -0400

    slugify urls and update markdownlint config

--- _config.ts
+++ _config.ts
@@ -1,10 +1,12 @@
 import lume from "lume/mod.ts";
+import slugifyUrls from "lume/plugins/slugify_urls.ts"
 import wiki from "wiki/mod.ts";
 
 const site = lume({
   src: "./src",
 });
 
+site.use(slugifyUrls());
 site.use(wiki());
 
 export default site;
